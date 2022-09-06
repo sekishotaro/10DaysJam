996d69284483ba4e3b1492249aaf505ddf622887
LC1B_12_YugoKuriyama <k020g1224@g.neec.ac.jp>
2022-09-06 14:54:19 +0900

    コミット漏れ

---
+++ Mapchip.cpp
@@ -0,0 +1,53 @@
+#include "Mapchip.h"
+
+//CSVファイル読み込み
+void Mapchip::CsvToVector(std::vector<std::vector<int>>& mapData, std::string fName) {
+	std::ifstream ifs(fName);//ファイルネームの確認
+	std::string line;
+	std::vector<int> csv;
+	while (getline(ifs, line))
+	{
+		std::istringstream stream(line);
+		std::string field;
+		std::vector<int> result;
+		while (std::getline(stream, field, ','))//カンマ区切りで
+		{
+			result.push_back(stoi(field));
+		}
+		for (auto i : result)
+		{
+			csv.push_back(i);
+		}
+	}
+	mapData.push_back(csv);
+}
+
+//マップナンバー指定
+int Mapchip::GetChipNum(int x, int y, std::vector<int> map)
+{
+	const int X = x / map_size;
+	const int Y = y / map_size;
+
+	if ((X < 0 || X >= map_max_x) || (Y < 0 || Y >= map_max_y))
+	{
+		return 0;
+	}
+
+	//ここの警告は気にしないでOK
+	return map[Y * map_max_x + X];
+}
+
+void Mapchip::SetChipNum(int x, int y, std::vector<int>& map)
+{
+	const int X = x / map_size;
+	const int Y = y / map_size;
+
+	if (map[Y * map_max_x + X] == 0)
+	{
+		map[Y * map_max_x + X] = 1;
+	}
+	else
+	{
+		map[Y * map_max_x + X] = 0;
+	}
+}
---
+++ Mapchip.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+//マップのサイズ(基本は1)
+const int map_size = 1;
+//縦横の最大数(マップによって変更)
+const int map_max_x = 104, map_max_y = 18;
+
+class Mapchip
+{
+public:
+	static void CsvToVector(std::vector<std::vector<int>>& mapData, std::string fName);
+
+	static int GetChipNum(int x, int y, std::vector<int> map);
+
+	static void SetChipNum(int x, int y, std::vector<int>& map);
+};
+
