5b64f0b9c9945d8ba603bbe3c10f0fa0b50b6058
taiga <k020g1219@g.neec.ac.jp>
2022-09-09 13:36:05 +0900

    シーン追加

---
+++ SelectScene.cpp
@@ -0,0 +1,47 @@
+#include "SelectScene.h"
+
+SelectScene::~SelectScene()
+{
+	Finalize();
+}
+
+void SelectScene::Initialize()
+{
+}
+
+void SelectScene::Finalize()
+{
+}
+
+void SelectScene::Update()
+{
+	Input* input = Input::GetInstance();
+}
+
+void SelectScene::Draw()
+{
+	ID3D12GraphicsCommandList* cmdList = DirectXCommon::GetInstance()->GetCmdList();
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+	// 背景スプライト描画
+
+	//sprite->Draw();
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	DirectXCommon::GetInstance()->ClearDepthBuffer();
+#pragma endregion 背景スプライト描画
+
+
+
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+#pragma endregion 前景スプライト描画
+}
--- scene/SceneFactory.cpp
+++ scene/SceneFactory.cpp
@@ -1,5 +1,5 @@
 #include "SceneFactory.h"
-
+#include "ResultScene.h"
 #include "GamePlayScene.h"
 #include "TitleScene.h"
 
@@ -16,6 +16,10 @@ BaseScene *SceneFactory::CreateScene(const std::string &sceneName)
     else if (sceneName == "GAMEPLAY")
     {
         newScene = new GamePlayScene();
+    }
+    else if (sceneName == "RESULT")
+    {
+        newScene = new ResultScene();
     }
 
     return newScene;
